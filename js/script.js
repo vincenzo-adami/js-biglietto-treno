d7e9c94cc1bd70bc18d801cd500419f8e47b8044
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-02 18:42:20 +0200

    UPDATE: delete typo, change varible name to english

--- js/script.js
+++ js/script.js
@@ -5,26 +5,25 @@ let distance = parseFloat(prompt('Quanti chilometri dovrai percorrere?'));
 let age = parseInt(prompt('Quanti anni ha il passeggero?'));
 
 // inizializzo il prezzo al kilometro
-const prezzoKm = 0.21;
+const priceKM = 0.21;
 
 // calcolo il prezzo finale
-let prezzoFinale = distance * prezzoKm;
+let finalPrice = distance * priceKM;
 
 // verifico la possibilità di sconto e nel caso l'applico
 if (age < 18) {
-  prezzoFinale = prezzoFinale * 0.8;
+  finalPrice = finalPrice * 0.8;
 }
 else if (age >= 65) {
-  10
-  prezzoFinale = prezzoFinale * 0.6;
+  finalPrice = finalPrice * 0.6;
 }
 
 // calcolo il prezzo con sole 2 cifre decimali
-let prezzoUmano = Math.round(prezzoFinale * 100) / 100;
+let humanPrice = Math.round(finalPrice * 100) / 100;
 
-console.log("Prezzo finale (con toFixed())" + prezzoFinale.toFixed(2));
+console.log("Prezzo finale (con toFixed())" + finalPrice.toFixed(2));
 
-console.log('Prezzo finale biglietto: €' + prezzoUmano);
+console.log('Prezzo finale biglietto: €' + humanPrice);
 
 document.getElementById('price').setAttribute("class", "");
-document.getElementById('price').innerHTML = 'Prezzo finale biglietto: €' + prezzoUmano;
+document.getElementById('price').innerHTML = 'Prezzo finale biglietto: €' + humanPrice;
